f72ff1108dac3d0012ebc6099bf15dbe83252719
NathanStein1 <nathanstein2@gmail.com>
2022-04-18 12:28:06 -0700

    parse

--- CODE/script.js
+++ CODE/script.js
@@ -1,12 +1,31 @@
 // Create time and date at top
 
 function updateTime(){
-    $("#currentDay").text(moment().format("ddd MM/D : hh:mm:ss"))
+    $("#currentDay").text(moment().format("ddd MM/D : hh:mm:ss A"))
 
 }
 setInterval(updateTime, 1000)
 
-// Allow blocks to be sortable
+// Change class name to reflect color changes, present == red etc
+// if(tabletime>#currentDay time) addClass("future")
 
 
-$(".sort").sortable();
+
+// Set time tds to moment.js?
+
+var tableTime = $('.hour')
+var tableTimeNum = parseInt(tableTime)
+
+
+
+console.log(tableTimeNum)
+
+
+// Save button to local storage
+var save = $('.saveBtn')
+
+save.on('click', function(){
+    console.log("Save to local")
+})
+
+
